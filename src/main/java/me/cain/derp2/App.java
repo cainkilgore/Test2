1ab0d868b9dc04f81b85779e0d15de3a3fc03ee0
Cain <cain.donaghey@live.co.uk>
2011-11-04 18:45:31 +0000

    Trololol

--- src/main/java/me/cain/derp2/App.java
+++ src/main/java/me/cain/derp2/App.java
@@ -4,10 +4,16 @@ package me.cain.derp2;
  * Hello world!
  *
  */
-public class App 
+public class App implements Runnable
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+	public void run()
+	{
+		// TODO Auto-generated method stub
+		
+	}
 }
